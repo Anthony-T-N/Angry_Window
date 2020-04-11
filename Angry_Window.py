3c54573a08f6078b2842af471e1d10bb3d531783
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2020-04-11 13:01:43 +0930

    Update Angry_Window.py

--- Angry_Window.py
+++ Angry_Window.py
@@ -3,18 +3,20 @@ import time
 # Blank window.
 root = Tk()
 root.geometry("100x100")
+root.configure(bg="black")
+#root.overrideredirect(True)   
 
 topFrame = Frame(root)
 topFrame.pack()
 
-bottomFrame = Frame(root)
-bottomFrame.pack(side=BOTTOM)
+#bottomFrame = Frame(root)
+#bottomFrame.pack(side=BOTTOM)
 
-welcome_text = Label(topFrame, text="Welcome")
-welcome_text.pack(side="bottom")
+#welcome_text = Label(topFrame, text="Welcome")
+#welcome_text.pack(side="bottom")
 
-contentplaceholder = Label(bottomFrame, text="Contentplaceholder")
-contentplaceholder.pack(side="bottom")
+#contentplaceholder = Label(bottomFrame, text="Contentplaceholder")
+#contentplaceholder.pack(side="bottom")
 
 variable = StringVar()
 
@@ -23,7 +25,7 @@ random_text = ["＼(^o^)／", "¯\_(ツ)_/¯", "\ (•◡•) /", "~(˘▾˘~)",
 def update_label():
     i = 0
     while 1:
-        time.sleep(1)
+        #time.sleep(1)
         if (i >= 4):
             i = 0
         variable.set(str(random_text[i]))
@@ -32,10 +34,12 @@ def update_label():
 
 
 face = Label(root, textvariable=variable)
+face.place(anchor=CENTER) 
 face.pack()
 start_button = Button(root, text="start", command=update_label)
 start_button.pack()
 
 # Keep running window
 root.mainloop()
+
 
